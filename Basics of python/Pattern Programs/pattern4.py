4caa1fff29559f94e8475c2b7ac24b3b159f5c27
Prakhar Agrawal <agrawalprakhar931@gmail.com>
2025-12-22 19:41:58 +0530

    Added pattern4 and pattern5

---
+++ Basics of python/Pattern Programs/pattern4.py
@@ -0,0 +1,14 @@
+for i in range(0,5):
+  for j in range(5,0,-1):
+    print(j, end=" ")
+  print()
+
+"""
+OUTPUT
+5 4 3 2 1 
+5 4 3 2 1 
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+
+"""
---
+++ Basics of python/Pattern Programs/pattern5.py
@@ -0,0 +1,16 @@
+for i in range(0,5):
+  ch = chr(ord('a') + i)
+  for j in range(0,5):
+    print(ch, end=" ")
+  print()
+
+
+"""
+OUTPUT
+a a a a a 
+b b b b b 
+c c c c c
+d d d d d
+e e e e e
+
+"""
