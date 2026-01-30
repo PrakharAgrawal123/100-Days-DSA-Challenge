afca6243923e93a850d12bd378d0883dca001c66
Prakhar Agrawal <agrawalprakhar931@gmail.com>
2026-01-30 07:10:02 +0530

    Added reverse_array

---
+++ Arrays/reverse_array.py
@@ -0,0 +1,13 @@
+def reverse_array(arr):
+    start = 0
+    end = len(arr) - 1
+
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+
+    return arr
+
+arr = [5, 4, 3, 2, 1]
+print(reverse_array(arr))
